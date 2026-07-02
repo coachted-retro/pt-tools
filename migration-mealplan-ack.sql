39a537820900ca73e60d5bb8ff8b518dabbaeab4
Ted Scholl <tscholl@termac.com>
2026-07-02 11:54:45 +0000

    Reschedule modal w/ TBD, NBA times+lead cleanup, gym-floor routing fix, weekly auto meal-plan cron for Complete/Complete+ w/ disclaimer+signature gate

---
+++ migration-mealplan-ack.sql
@@ -0,0 +1,2 @@
+ALTER TABLE meal_plans ADD COLUMN ack_signature TEXT;
+ALTER TABLE meal_plans ADD COLUMN ack_at TEXT;
---
+++ migration-reschedule.sql
@@ -0,0 +1,3 @@
+ALTER TABLE appointment_status ADD COLUMN reschedule_date TEXT;
+ALTER TABLE appointment_status ADD COLUMN reschedule_time TEXT;
+ALTER TABLE appointment_status ADD COLUMN reschedule_tbd INTEGER;
